94fdc1b9c7d5bfafdb7e4facee1973fb0c0fdf31
nexlangpy-star <nexlangpy@gmail.com>
2026-08-21 01:53:02 +0300

    Delete Lib_Devil directory

--- Lib_Devil/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-from .TOP import *
-lib_dir = os.path.dirname(__file__)
-os.environ['LD_LIBRARY_PATH'] = lib_dir + ':' + os.environ.get('LD_LIBRARY_PATH', '')
-
-if lib_dir not in sys.path:
-    sys.path.insert(0, lib_dir)
-__version__ = "1.0.0"
